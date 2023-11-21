356bc27318834b0a749ce3144d25a8f92a55245a
weedenrj <robertweeden1997@gmail.com>
2023-11-20 19:07:49 -0600

    update more seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,16 @@ export default function Home() {
       title: "The Red Shed | Madison, Wi.",
       url: "https://redshedmadison.com",
       image: "https://redshedmadison.com/logo.png",
-      desc: "Fill in your SEO"
+      desc: "The Red Shed is open every weekend until 2 pm."
     }}>
       {/* <Navbar /> */}
 
 
-      <div className="bg-black flex flex-col h-full w-full justify-center items-center gap-4 p-4 font-mono overflow-hidden bg-black text-white md:bg-white md:text-black">
+      <div className="flex flex-col h-full w-full justify-center items-center gap-4 p-4 font-mono overflow-hidden">
         <div className='relative flex flex-col justify-center items-center'>
-          <div className="absolute top-0 -left-4 w-72 h-72 bg-blue rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob" />
-          <div className="absolute top-0 -right-4 w-72 h-72 bg-sand md:bg-orange rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-2000" />
-          <div className="absolute -bottom-8 left-20 w-72 h-72 bg-sand md:bg-yellow rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000" />
+          <div className="absolute top-0 -left-4 w-72 h-72 bg-red rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob" />
+          <div className="absolute top-0 -right-4 w-72 h-72 bg-blue-600 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-2000" />
+          <div className="absolute -bottom-8 left-20 w-72 h-72 bg-yellow rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000" />
 
           <p className='md:relative md:z-10 text-3xl md:text-5xl'>
             👷Oops!⛔
