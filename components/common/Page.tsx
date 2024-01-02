d29738cfe79bceda9aeca0d02426262e7a22ed1a
weedenrj <robertweeden1997@gmail.com>
2024-01-01 21:38:28 -0600

    viewport change again

--- components/common/Page.tsx
+++ components/common/Page.tsx
@@ -15,7 +15,7 @@ export default function Page({
   return (
     <>
       <Helmet {...SEO} />
-      <div className={clsx("relative h-lvh w-screen overflow-x-hidden bg-black")}>
+      <div className={clsx("relative h-svh w-screen overflow-x-hidden bg-black")}>
         {children}
       </div>
     </>
