519b96e214cf1a4be1fddec3550717c2b608d563
Rob Weeden <rob@4-cube.io>
2024-01-09 19:49:25 -0600

    building route

---
+++ app/building/page.tsx
@@ -0,0 +1,33 @@
+import Navbar from "@components/common/Navbar"
+import Page from "@components/common/Page"
+import React from "react"
+import Hero from "@components/sections/Hero"
+import About from "@components/sections/About"
+import Specialties from "@components/sections/Specialties"
+import Atmosphere from "@components/sections/Atmosphere"
+import Location from "@components/sections/Location"
+import { type Viewport } from "next"
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "#a80505" },
+  ],
+  width: "device-width",
+  initialScale: 1,
+}
+
+export default function Home() {
+  return (
+    <Page>
+      <Navbar />
+      <Hero />
+      <div className="flex flex-col gap-24 xs:gap-40 sm:gap-[200px]">
+        <About />
+        <Specialties />
+        <Atmosphere />
+        <Location />
+      </div>
+    </Page>
+  )
+}
