4137a048500a390c8b5aa1f9795207d0bf1ff748
Rob Weeden <rob@4-cube.io>
2024-02-23 16:34:45 -0600

    see if linking to page header fixes browser bar issues

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer({ className, ...rest }: FooterProps) {
       </div>
 
       <div className="flex flex-col w-full sm:flex-row items-center sm:justify-between gap-12 px-10 md:px-20">
-        <Link href={mkNavLink("/#hero")} className="transition-transform hover:scale-102 active:animate-pop">
+        <Link href={mkNavLink("/#page-header")} className="transition-transform hover:scale-102 active:animate-pop">
           <Image
             alt="The Red Shed Logo"
             src="/RedShedLogoFacelift.webp"
