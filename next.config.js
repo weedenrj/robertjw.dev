db095a55f7cd6f272b41a3142186affd4d6ba70d
Rob Weeden <rob@4-cube.io>
2024-02-23 15:18:42 -0600

    update next image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "assets.tina.io"
+    }]
+  }
 }
 
 module.exports = nextConfig
