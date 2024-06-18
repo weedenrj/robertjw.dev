f3df72426c19908844ef412bb959622919066b1c
Rob Weeden <rob@4-cube.io>
2024-06-18 17:05:40 -0500

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
         </h3>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-2">
           {skills.map((item) =>
-            <SkillCard skill={item} />
+            <SkillCard key={item.id} skill={item} />
           )}
         </div>
       </div>
